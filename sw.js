60c33a4d5cf9de09d216ece9e660928029eb4c3f
Juan Diego <20223tn127@utez.edu.mx>
2025-10-09 12:24:39 -0600

    [FIRST]- Primer Commit

---
+++ sw.js
@@ -0,0 +1,27 @@
+const STATIC_CACHE_NAME = 'app-shell-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(STATIC_CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+  console.log('[SW] Instalado y App Shell precacheado');
+});
+
+self.addEventListener('fetch', event => {
+  const urlPath = new URL(event.request.url).pathname;
+  const isAppShellResource = ASSETS.includes(urlPath);
+
+  if (isAppShellResource) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => response)
+    );
+  }
+});
